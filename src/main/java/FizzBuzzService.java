6a36aa9ee90e1875dc3aa393a632fd8867526b5c
SunBangchao <>
2020-01-10 22:27:19 +0800

    optimize

--- src/main/java/FizzBuzzService.java
+++ src/main/java/FizzBuzzService.java
@@ -2,35 +2,26 @@ import javafx.beans.binding.When;
 
 public class FizzBuzzService {
 
-    public static final String FIZZ = "Fizz";
-    public static final String BUZZ = "Buzz";
-    public static final String WHIZZ = "Whizz";
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String WHIZZ = "Whizz";
 
     public String fizzBuzz(int index){
         StringBuffer result = new StringBuffer("");
         if(containNumber(index,3) && !containNumber(index,5)){
             result.append(FIZZ);
         }else {
-            if (multiply3(index) && (!containNumber(index,5) || containNumber(index,7))) result.append(FIZZ);
-            if (multiply5(index) && !containNumber(index,7)) result.append(BUZZ);
-            if (multiply7(index)) result.append(WHIZZ);
+            if (multiplyByNumber(index,3) && (!containNumber(index,5) || containNumber(index,7))) result.append(FIZZ);
+            if (multiplyByNumber(index,5) && !containNumber(index,7)) result.append(BUZZ);
+            if (multiplyByNumber(index,7)) result.append(WHIZZ);
             if (result.length() == 0) result.append(index);
         }
         return result.toString();
     }
 
-    private boolean multiply3(int index){
-        if(index % 3 == 0) return true;
-        return false;
-    }
-
-    private boolean multiply5(int index){
-        if(index % 5 == 0) return true;
-        return false;
-    }
-
-    private boolean multiply7(int index){
-        if(index % 7 == 0) return true;
+    private boolean multiplyByNumber(int number1, int number2){
+        if(number2 == 0) return number1 == 0;
+        if(number1 % number2 == 0) return true;
         return false;
     }
 
