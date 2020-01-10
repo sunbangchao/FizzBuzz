238ab7b258d1f43b2abcb02f9f483a558fbe4c4c
SunBangchao <>
2020-01-10 15:39:10 +0800

    passed testcase : multiply_test

--- src/main/java/FizzBuzzService.java
+++ src/main/java/FizzBuzzService.java
@@ -1,6 +1,26 @@
 public class FizzBuzzService {
 
     public String fizzBuzz(int index){
-        return null;
+        StringBuffer result = new StringBuffer("");
+        if(multiply3(index)) result.append("Fizz");
+        if(multiply5(index)) result.append("Buzz");
+        if(multiply7(index)) result.append("Whizz");
+        if(result.length() == 0) result.append(index);
+        return result.toString();
+    }
+
+    private boolean multiply3(int index){
+        if(index % 3 == 0) return true;
+        return false;
+    }
+
+    private boolean multiply5(int index){
+        if(index % 5 == 0) return true;
+        return false;
+    }
+
+    private boolean multiply7(int index){
+        if(index % 7 == 0) return true;
+        return false;
     }
 }
