a77c40c79b1d7680f3426a5dc6e360222814b24c
SunBangchao <>
2020-01-10 15:55:49 +0800

    passed testcase : contain5,7

--- src/main/java/FizzBuzzService.java
+++ src/main/java/FizzBuzzService.java
@@ -2,7 +2,11 @@ public class FizzBuzzService {
 
     public String fizzBuzz(int index){
         StringBuffer result = new StringBuffer("");
-        if(contain3(index)){
+        if(contain7(index)){
+            result.append("Flzz");
+        }else if(contain5(index)){
+            result.append("BuzzWhizz");
+        }else if(contain3(index)){
             result.append("Fizz");
         }else {
             if (multiply3(index)) result.append("Fizz");
@@ -32,6 +36,24 @@ public class FizzBuzzService {
         while (index > 0){
             int num = index % 10;
             if (num == 3) return true;
+            index = index / 10;
+        }
+        return false;
+    }
+
+    private boolean contain5(int index){
+        while (index > 0){
+            int num = index % 10;
+            if (num == 5) return true;
+            index = index / 10;
+        }
+        return false;
+    }
+
+    private boolean contain7(int index){
+        while (index > 0){
+            int num = index % 10;
+            if (num == 7) return true;
             index = index / 10;
         }
         return false;
