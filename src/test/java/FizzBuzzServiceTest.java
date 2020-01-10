cc6006b7fb38d9109fd64c6846982fe2a16e924b
SunBangchao <>
2020-01-10 15:36:06 +0800

    testcase : multiply_test

--- src/test/java/FizzBuzzServiceTest.java
+++ src/test/java/FizzBuzzServiceTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,20 @@ public class FizzBuzzServiceTest {
     static void init(){
         service = new FizzBuzzService();
     }
-    
+
+
+
+    @Test
+    void multiply_test(){
+        //2,3,5,7,15,21,35,105
+        Assertions.assertEquals("2",service.fizzBuzz(2));
+        Assertions.assertEquals("Fizz",service.fizzBuzz(3));
+        Assertions.assertEquals("Buzz",service.fizzBuzz(5));
+        Assertions.assertEquals("Whizz",service.fizzBuzz(7));
+        Assertions.assertEquals("FizzBuzz",service.fizzBuzz(15));
+        Assertions.assertEquals("FizzWhizz",service.fizzBuzz(21));
+        Assertions.assertEquals("BuzzWhizz",service.fizzBuzz(35));
+        Assertions.assertEquals("FizzBuzzWhizz",service.fizzBuzz(105));
+    }
 
 }
