cf313f6267eda120eddcaade87b6808895832292
SunBangchao <>
2020-01-10 17:10:59 +0800

    update testcase : contain5,7

--- src/test/java/FizzBuzzServiceTest.java
+++ src/test/java/FizzBuzzServiceTest.java
@@ -16,16 +16,16 @@ public class FizzBuzzServiceTest {
 
     @Test
     void multiply_test(){
-        //2,3,5,7,15,21,70,105
+        //2,3,5,7,21,70
         Assertions.assertEquals("2",service.fizzBuzz(2));
         Assertions.assertEquals("Fizz",service.fizzBuzz(3));
         Assertions.assertEquals("Buzz",service.fizzBuzz(5));
         Assertions.assertEquals("Whizz",service.fizzBuzz(7));
-        Assertions.assertEquals("FizzBuzz",service.fizzBuzz(15));
+//        Assertions.assertEquals("FizzBuzz",service.fizzBuzz(15));
         Assertions.assertEquals("FizzWhizz",service.fizzBuzz(21));
 //        Assertions.assertEquals("BuzzWhizz",service.fizzBuzz(35));
         Assertions.assertEquals("BuzzWhizz",service.fizzBuzz(70));
-        Assertions.assertEquals("FizzBuzzWhizz",service.fizzBuzz(105));
+//        Assertions.assertEquals("FizzBuzzWhizz",service.fizzBuzz(105));
     }
 
     @Test
@@ -39,9 +39,10 @@ public class FizzBuzzServiceTest {
     @Test
     void contain5_test(){
         //包含5时，忽略包含3和被3整除
-        //15,35,65
+        //15,35,65,105
         Assertions.assertEquals("Buzz",service.fizzBuzz(15));
         Assertions.assertEquals("BuzzWhizz",service.fizzBuzz(35));
+        Assertions.assertEquals("BuzzWhizz",service.fizzBuzz(105));
     }
 
     @Test
