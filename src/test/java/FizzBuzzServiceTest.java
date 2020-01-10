c03d4d4c2d121d5fe958ca09d087acc369152c8f
SunBangchao <>
2020-01-10 15:51:03 +0800

    testcase : contain5,7

--- src/test/java/FizzBuzzServiceTest.java
+++ src/test/java/FizzBuzzServiceTest.java
@@ -16,24 +16,41 @@ public class FizzBuzzServiceTest {
 
     @Test
     void multiply_test(){
-        //2,3,5,7,15,21,70,105
+        //2,3,5,7,21
         Assertions.assertEquals("2",service.fizzBuzz(2));
         Assertions.assertEquals("Fizz",service.fizzBuzz(3));
         Assertions.assertEquals("Buzz",service.fizzBuzz(5));
         Assertions.assertEquals("Whizz",service.fizzBuzz(7));
-        Assertions.assertEquals("FizzBuzz",service.fizzBuzz(15));
+//        Assertions.assertEquals("FizzBuzz",service.fizzBuzz(15));
+        Assertions.assertEquals("FizzBuzz",service.fizzBuzz(60));
         Assertions.assertEquals("FizzWhizz",service.fizzBuzz(21));
 //        Assertions.assertEquals("BuzzWhizz",service.fizzBuzz(35));
-        Assertions.assertEquals("BuzzWhizz",service.fizzBuzz(70));
-        Assertions.assertEquals("FizzBuzzWhizz",service.fizzBuzz(105));
+//        Assertions.assertEquals("FizzBuzzWhizz",service.fizzBuzz(105));
     }
 
     @Test
     void contain3_test(){
-        //13,30,35
+        //13,30
         Assertions.assertEquals("Fizz",service.fizzBuzz(13));
         Assertions.assertEquals("Fizz",service.fizzBuzz(30));
-        Assertions.assertEquals("Fizz",service.fizzBuzz(35));
+//        Assertions.assertEquals("Fizz",service.fizzBuzz(35));
+    }
+
+    @Test
+    void contain5_test(){
+        //35,15,105
+        Assertions.assertEquals("BuzzWhizz",service.fizzBuzz(35));
+        Assertions.assertEquals("BuzzWhizz",service.fizzBuzz(15));
+        Assertions.assertEquals("BuzzWhizz",service.fizzBuzz(105));
+    }
+
+    @Test
+    void contain7_test(){
+        //75,70,17,107
+        Assertions.assertEquals("Flzz",service.fizzBuzz(75));
+        Assertions.assertEquals("Flzz",service.fizzBuzz(70));
+        Assertions.assertEquals("Flzz",service.fizzBuzz(17));
+        Assertions.assertEquals("Flzz",service.fizzBuzz(107));
     }
 
 }
